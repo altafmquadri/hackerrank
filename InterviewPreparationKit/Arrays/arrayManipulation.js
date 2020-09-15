60770d366135651b793081d9cde47df4cacf8a42
Altaf Quadri <altafmquadri@gmail.com>
2020-09-15 15:30:50 -0400

    refactored counting valleys and add arrayManipulation

---
+++ InterviewPreparationKit/Arrays/arrayManipulation.js
@@ -0,0 +1,148 @@
+/* Starting with a 1-indexed array of zeros and a list of operations, for each operation add 
+a value to each of the array element between two given indices, inclusive. Once all operations 
+have been performed, return the maximum value in the array.
+
+Example
+Queries are interpreted as follows:
+    a b k
+    1 5 3
+    4 8 7
+    6 9 1
+
+Add the values of  between the indices  and  inclusive:
+
+index->	 1 2 3  4  5 6 7 8 9 10
+    [0,0,0, 0, 0,0,0,0,0, 0]
+    [3,3,3, 3, 3,0,0,0,0, 0]
+    [3,3,3,10,10,7,7,7,0, 0]
+    [3,3,3,10,10,8,8,8,1, 0]
+The largest value is  after all operations are performed.
+
+Function Description
+Complete the function arrayManipulation in the editor below.
+arrayManipulation has the following parameters:
+
+int n - the number of elements in the array
+int queries[q][3] - a two dimensional array of queries where each queries[i] contains three integers, a, b, and k.
+Returns
+
+int - the maximum value in the resultant array
+Input Format
+
+The first line contains two space-separated integers  and , the size of the array and the number of operations.
+Each of the next  lines contains three space-separated integers ,  and , the left index, right index and summand.
+
+Constraints
+
+Sample Input
+
+5 3
+1 2 100
+2 5 100
+3 4 100
+Sample Output
+
+200
+Explanation
+
+After the first update the list is 100 100 0 0 0.
+After the second update list is 100 200 100 100 100.
+After the third update list is 100 200 200 200 100.
+
+The maximum value is 200. */
+
+/* 
+Brute force
+
+const arrayManipulation = (n, queries) => {
+    //create the arr
+    let rows = queries.length
+    let cols = n
+    let currentRow, leftIndex, rightIndex, summand, fillIndex
+    let sum = 0, max = 0
+    const arr = Array.from(new Array(rows), () => Array(cols).fill(0))
+
+    //fill in the array values with range (leftIndex to rightIndex) with the summand
+    for (let row = 0; row < rows; row++) {
+        currentRow = []
+        for (let col = 0; col < cols; col++) {
+            currentRow.push(queries[row][col])
+        }
+
+        leftIndex = currentRow[0] - 1
+        rightIndex = currentRow[1]
+        summand = currentRow[2]
+        fillIndex = leftIndex
+
+        while (fillIndex < rightIndex) {
+            arr[row][fillIndex] += summand
+            fillIndex++
+        }
+    }
+
+    for (let col = 0; col < cols; col++) {
+        let i = 0
+
+        while (i < rows) {
+            sum += arr[i][col]
+            i++
+        }
+        max = Math.max(sum, max)
+        sum = 0
+    }
+    return max
+} */
+
+
+/* 
+Brute force refactored with 1D array and less space complexity
+
+const arrayManipulation = (n, queries) => {
+    //create the arr
+    let cols = n
+    const arr = new Array(cols).fill(0)
+    let qRow, leftIndex, rightIndex, summand, max
+
+    //fill in the array values with range (leftIndex to rightIndex) with the summand
+    for (let query of queries) {
+        qRow = query
+        leftIndex = qRow[0] - 1
+        rightIndex = qRow[1]
+        summand = qRow[2]
+
+        while (leftIndex < rightIndex) {
+            arr[leftIndex] += summand
+            leftIndex++
+        }
+    }
+    max = Math.max(...arr)
+    return max
+} */
+
+
+//optimal solution O(m + n)
+const arrayManipulation = (n, queries) => {
+    const cols = n
+    const arr = new Array(n).fill(0)
+    let qRow, leftIndex, rightIndex, summand
+
+    for (const query of queries) {
+        qRow = query
+        leftIndex = qRow[0] - 1
+        rightIndex = qRow[1]
+        summand = qRow[2]
+        arr[leftIndex] += summand
+        if (rightIndex < arr.length) arr[rightIndex] -= summand
+    }
+    let sum = 0, max = 0
+
+    for (let [i, val] of arr.entries()) {
+        sum += val
+        max = Math.max(sum, max)
+    }
+    return max
+}
+
+console.log(arrayManipulation(5, [[1, 2, 100], [2, 5, 100], [3, 4, 100]])) //200
+console.log(arrayManipulation(10, [[1, 5, 3], [4, 8, 7], [6, 9, 1]])) //10
+console.log(arrayManipulation(10, [[2, 6, 8], [3, 5, 7], [1, 8, 1], [5, 9, 15]])) //31
--- InterviewPreparationKit/warmUpChallenges/countingValleys.js
+++ InterviewPreparationKit/warmUpChallenges/countingValleys.js
@@ -45,23 +45,38 @@ _/\      _
     \/\/
 It's clear that there is only one valley there, so we print on a new line. */
 
+// const countingValleys = (n, s) => {
+//     //assign a value to each letter
+//     const arr = [...s].map(letter => letter === 'U' ? 1 : -1)
+//     let count = 0, sum = 0
+//     //create a running total of all the ups and downs 
+//     const sumArray = []
+//     for (let num of arr) {
+//         sumArray.push(sum += num)
+//     }
+//     //in the running total check to see if there is a point where zero is reached
+//     //if it is check the position right before to see that it is an upward movement, if so count it as a valley
+//     for (let [i, num] of sumArray.entries()) {
+//         if (num === 0 && sumArray[i - 1] === -1) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+//refactored
 const countingValleys = (n, s) => {
-    //assign a value to each letter
-    const arr = [...s].map(letter => letter === 'U' ? 1 : -1)
     let count = 0, sum = 0
-    //create a running total of all the ups and downs 
-    const sumArray = []
-    for (let num of arr) {
-        sumArray.push(sum += num)
-    }
-    //in the running total check to see if there is a point where zero is reached
-    //if it is check the position right before to see that it is an upward movement, if so count it as a valley
-    for (let [i, num] of sumArray.entries()) {
-        if (num === 0 && sumArray[i - 1] === -1) {
-            count++
-        }
-    }
+    //assign string a number either 1 or -1 and then convert to running total
+    const path = [...s].map(val => val === 'U' ? 1 : -1)
+        .map(val => sum += val)
+    //check for a zero value, if left value is -1 they are coming from below to sea-level count as valley
+    path.forEach((val, i) => {
+        val === 0 && path[i - 1] === -1 ? count++ : false
+    })
     return count
 }
 
+//cSpell:disable
 console.log(countingValleys(8, 'UDDDUDUU'));
+console.log(countingValleys(12, 'DDUUDDUDUUUD'))
